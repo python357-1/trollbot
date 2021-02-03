43f3e77542edf567be2f738b3cab1ed3039c2ddc
python357-1 <jb2101554@gmail.com>
2021-02-02 19:23:56 -0600

    fixed play command

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ require('dotenv').config()
 server.on("error", (err) => {
   console.error(err)
 })
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('type "trollbot help" if u dum lmao')
 });
 
-let activePlayers = [];
 let playerMap = new Map()
 playerMap['python357'] = ['https://i.imgur.com/kU1brlO.png']
 playerMap['powerboyNL'] = [
@@ -36,37 +36,43 @@ playerMap['cbrl'] = [
 
 client.on('message', msg => {
   if (msg.content === 'trollbot play') {
-    if (activePlayers.indexOf(msg.author.username) != -1) {
-      let imageArr = playerMap[activePlayers[Math.round(Math.random() * (activePlayers.length - 1))]]
-      let image = imageArr[Math.round(Math.random() * (imageArr.length - 1))]
-      msg.channel.send(image)
-    } else {
-      msg.channel.send('try being in the game, loser')
-    }
+    server.lrange(msg.guild.id, 0, -1, (err, res) => {
+      if (err) {
+        console.error(err)
+      }
+      if (res.indexOf(msg.author.username) != -1) {
+        let imageArr = playerMap[res[Math.random() * (res.length - 1)]]
+        if (typeof imageArr != "object") {
+          msg.channel.send('try being popular lmao')
+        } else {
+          let image = imageArr[Math.round(Math.random() * (imageArr.length - 1))]
+          msg.channel.send(image)
+        }
+      } else {
+        msg.channel.send('try being in the game, loser')
+      }
+    })
   } else if (msg.content === "trollbot addme") {
     let player = msg.author.username
     server.rpush(msg.guild.id, msg.author.username)
-    server.lrange(msg.guild.id, 0, -1, redis.print)
     msg.channel.send(`${player} has joined the game!`)
   } else if (msg.content === "trollbot clear") {
     server.del(msg.guild.id)
     msg.channel.send('Players cleared!')
   } else if (msg.content === "trollbot show") {
     server.lrange(msg.guild.id, 0, -1, (err, res) => {
-      active_players = res
       if (err) {
         console.error(err)
       }
-      console.log(res)
 
-      if (active_players.length < 1) {
+      if (res.length < 1) {
         if (msg.author.username == "python357") {
           msg.channel.send('nobody\'s playing, father')
         } else {
           msg.channel.send('nobody\'s playing bitch')
         }
       } else {
-        msg.channel.send(`Active Players: ${active_players.join(', ')}`)
+        msg.channel.send(`Active Players: ${res.join(', ')}`)
       }
     })
   } else if (msg.content === "trollbot help") {
